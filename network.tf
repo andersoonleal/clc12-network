fd1c0840b9367f28fc9ad7e998b0692f9572cf06
EC2 Default User <ec2-user@ip-172-31-2-102.ec2.internal>
2024-11-11 04:30:37 +0000

    update network

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 variable "vpc_name" {
-    type = string
+    type = string 
     default = "vpc_clc12_terraform_iac"
 }
 
@@ -17,7 +17,7 @@ resource "aws_subnet" "public_subnet" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "public_subnet_1a"
+    Name = "tf_public_subnet_1a"
   }
 }
 
@@ -27,7 +27,7 @@ resource "aws_subnet" "private_subnet" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "private_subnet_1a"
+    Name = "tf_private_subnet_1a"
   }
 }
 
@@ -56,24 +56,25 @@ resource "aws_route_table_association" "public_rt_associate" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
-  
+
 resource "aws_eip" "nat_gw_ip" {
   domain   = "vpc"
 }
 
 resource "aws_nat_gateway" "natgw_1a" {
-      allocation_id = aws_eip.nat_gw_ip.id
-      subnet_id = aws_subnet.public_subnet.id
-      
-      tags = {
-        Name = "tf_natgw"
-      }
-  
-      depends_on = [aws_internet_gateway.gw]
-      
+  allocation_id = aws_eip.nat_gw_ip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "tf_natgw"
   }
-  
-  resource "aws_route_table" "private_rt" {
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.minha_vpc.id
 
   route {
@@ -87,6 +88,50 @@ resource "aws_nat_gateway" "natgw_1a" {
 }
 
 resource "aws_route_table_association" "private_rt_associate" {
-    subnet_id = aws_subnet.private_susSbnet.id
-    route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+###Criando instância EC2
+
+module "app_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "App-SG"
+  description = "App SG for web app"
+  vpc_id      = aws_vpc.minha_vpc.id
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "Porta para minha aplicacao web de acesso"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  egress_rules        = ["all-all"]
+}
+
+module "ec2_app_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "Web-App-Instance"
+
+  ami                    = "ami-06b21ccaeff8cd686"
+  instance_type          = "t2.micro"
+  key_name               = "vockey"
+  monitoring             = true
+  vpc_security_group_ids = [module.app_sg.security_group_id]
+  subnet_id              = aws_subnet.public_subnet.id
+  user_data              = file("./dependencias.sh")
+}
+
+resource "aws_eip" "ec2_app_instance_ip" {
+  instance = module.ec2_app_instance.id
+  domain = "vpc"
+
+  tags = {
+    Name = "Web-Server-EIP"
+  }
 }
